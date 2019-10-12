7ff469c0293a23afcf6c44ba6cccf6f911ccb67e
lenin-8016 <lenin.m@zohocorp.com>
2019-10-12 16:13:52 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  { path: 'content', loadChildren: './tab1/content/content.module#ContentPageModule' },
   { path: 'home', loadChildren: './tab2/home/home.module#HomePageModule' },
   { path: 'new-item', loadChildren: './tab2/new-item/new-item.module#NewItemPageModule' },
   { path: 'update-item/:id', loadChildren: './tab2/update-item/update-item.module#UpdateItemPageModule' },
